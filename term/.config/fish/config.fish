db7af2bdc42eb50a6301b09d20aad056514e6ad3
Taylor Snead <taylorsnead@gmail.com>
2019-10-10 21:24:56 -0400

    :fish: Clean out fish files

--- term/.config/fish/config.fish
+++ term/.config/fish/config.fish
@@ -2,3 +2,10 @@
 cat ~/.config/wpg/sequences &
 
 source ~/.config/wpg/templates/colors.fish
+
+# Install all fish packages on new systems!
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
