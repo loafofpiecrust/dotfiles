a77e393dee1490c901f09b037abcb2bc0723f2fd
Taylor Snead <taylor@snead.xyz>
2021-01-16 11:31:10 -0500

    Fix system password prompts

--- scripts/bin/get-password.sh
+++ scripts/bin/get-password.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-content=$(emacsclient -ne "(bitwarden-get-password \"$1\" \"$2\")")
+content=$(emacsclient -ne "(bitwarden-getpass-for-user \"$1\" \"$2\")")
 echo "${content:1:-1}"
